c45baad4b9dff9ba9c93c4c772dea0b5c1865070
Alston16-c <jonathanalston831@gmail.com>
2026-03-08 20:34:08 +0700

    Add files via upload

---
+++ template/template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define int long long  
+const int INF = 1e18;
+
+void solve() {
+    
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}                                                
