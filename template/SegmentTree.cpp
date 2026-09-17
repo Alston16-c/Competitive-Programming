23b68cd806e9e52b64fe3ed5824688fd259d9953
Alston16-c <jonathanalston831@gmail.com>
2026-09-17 10:40:13 +0700

    Update SegmentTree.cpp

--- template/SegmentTree.cpp
+++ template/SegmentTree.cpp
@@ -1,77 +1,107 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<class S, S (*op)(S, S), S (*e)()>
 struct SegTree {
-    int _n, size, log;
-    vector<S> d;
+    int n;
+    vector<int> seg;
 
-    SegTree(int n) {
-        _n = n;
-        log = 0;
-        while ((1 << log) < n) log++;
-        size = 1 << log;
-        d.assign(2 * size, e());
+    SegTree(const vector<int>& a) : n(a.size()), seg(4 * n + 5) {
+        build(0, 0, n - 1, a);
     }
 
-    SegTree(const vector<S>& v) : SegTree(v.size()) {
-        for (int i = 0; i < (int)v.size(); i++)
-            d[size + i] = v[i];
-        build();
+    int combine(int a, int b) {
+        return max(a, b);
     }
 
-    void build() {
-        for (int i = size - 1; i >= 1; i--)
-            d[i] = op(d[2 * i], d[2 * i + 1]);
+    int query(int ql, int qr) {
+        return query(0, 0, n - 1, ql, qr);
     }
 
-    void set(int p, S x) {
-        p += size;
-        d[p] = x;
-        for (p >>= 1; p >= 1; p >>= 1)
-            d[p] = op(d[2 * p], d[2 * p + 1]);
+    void update(int idx, int val) {
+        update(0, 0, n - 1, idx, val);
     }
 
-    S get(int p) {
-        return d[p + size];
+    int walk(int ql, int x) {
+        return walk(0, 0, n - 1, ql, x);
     }
 
-    S prod(int l, int r) {
-        S sml = e(), smr = e();
-        l += size;
-        r += size;
-
-        while (l < r) {
-            if (l & 1) sml = op(sml, d[l++]);
-            if (r & 1) smr = op(d[--r], smr);
-            l >>= 1;
-            r >>= 1;
+private:
+    void build(int curr, int l, int r, const vector<int>& a) {
+        if (l == r) {
+            seg[curr] = a[l];
+            return;
         }
 
-        return op(sml, smr);
+        int mid = (l + r) / 2;
+
+        build(2 * curr + 1, l, mid, a);
+        build(2 * curr + 2, mid + 1, r, a);
+
+        seg[curr] = combine(seg[2 * curr + 1], seg[2 * curr + 2]);
     }
 
-    S all_prod() {
-        return d[1];
+    int query(int curr, int l, int r, int ql, int qr) {
+        if (r < ql || l > qr) return 0;
+        if (ql <= l && r <= qr) return seg[curr];
+
+        int mid = (l + r) / 2;
+
+        return combine(
+            query(2 * curr + 1, l, mid, ql, qr),
+            query(2 * curr + 2, mid + 1, r, ql, qr)
+        );
+    }
+
+    void update(int curr, int l, int r, int idx, int val) {
+        if (l == r) {
+            seg[curr] = val;
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (idx <= mid)
+            update(2 * curr + 1, l, mid, idx, val);
+        else
+            update(2 * curr + 2, mid + 1, r, idx, val);
+
+        seg[curr] = combine(seg[2 * curr + 1], seg[2 * curr + 2]);
+    }
+
+    int walk(int curr, int l, int r, int ql, int x) {
+        if (r < ql || seg[curr] < x) return -1;
+        if (l == r) return l;
+
+        int mid = (l + r) / 2;
+
+        int res = walk(2 * curr + 1, l, mid, ql, x);
+
+        if (res != -1) return res;
+
+        return walk(2 * curr + 2, mid + 1, r, ql, x);
     }
 };
 
-int op(int a, int b) {
-    return a + b;
-}
-
-int e() {
-    return 0;
-}
-
 int main() {
-    vector<int> v = {1,2,3,4,5};
+    vector<int> a = {2, 7, 1, 9, 4, 6, 3};
 
-    SegTree<int, op, e> seg(v);
+    SegTree st(a);
 
-    cout << seg.prod(1,4) << endl; // 2+3+4 = 9
+    // Range maximum
+    cout << st.query(1, 4) << '\n';
+    // max(7, 1, 9, 4) = 9
 
-    seg.set(2,10);
+    // Point update: a[2] = 10
+    st.update(2, 10);
 
-    cout << seg.prod(1,4) << endl; // 2+10+4 = 16
+    cout << st.query(1, 4) << '\n';
+    // max(7, 10, 9, 4) = 10
+
+    // Find first index >= 3 whose value >= 8
+    cout << st.walk(3, 8) << '\n';
+    // index 3, because a[3] = 9
+
+    // Find first index >= 4 whose value >= 8
+    cout << st.walk(4, 8) << '\n';
+    // -1
 }
